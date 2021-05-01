5a7ef9f5471c0de932785aaf177e6b2ad752f0b5
Om Dahale <om.g.dahale@gmail.com>
2021-05-01 09:50:53 +0530

    style contact form

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Add static file directory
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
