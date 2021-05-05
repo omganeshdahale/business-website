bc45d3d35618cca2c2721a29b032a2de97045973
Om Dahale <om.g.dahale@gmail.com>
2021-05-05 11:14:14 +0530

    show columns in change list

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class Contact(models.Model):
     email = models.EmailField(max_length=100)
 
     def __str__(self):
-        return f'{self.date_created} - {self.name}'
+        return f'{self.date_created:%b %-d, %Y} : {self.name}'
 
     class Meta:
         abstract = True
